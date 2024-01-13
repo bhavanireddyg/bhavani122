5b43a7caddbbdfaba8872225f61f33e835ac3eff
bhavanireddyg <bhavani.reddy2021@vitstudent.ac.in>
2024-01-13 17:26:13 +0530

    added bhavani12 program

---
+++ bhavani12.java
@@ -0,0 +1,5 @@
+public class bhavani12 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! bhavani reddy here....... holding registration number 21MIC0122");
+    }
+}
